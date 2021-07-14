274830ddad35371cb9eea89930b247c765fa1c54
nortonlui <nortonlui@gmail.com>
2021-07-14 09:10:21 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function getDate() {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0');
+  let yyyy = today.getFullYear();
+  today = dd + '/' + mm + '/' + yyyy;
+  return {
+    today,
+    day: dd,
+    month: mm,
+    year: yyyy,
+  };
+}
